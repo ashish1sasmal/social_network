251b110580ed477de44055eb06c147f74e670e5d
ashish1sasmal <ashishsasmal1@gmail.com>
2019-09-02 21:58:47 +0530

    fuctional login

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     
-    path('login_signup/',views.signup,name='login-signup'),
+    path('login_signup/',views.login_signup,name='login-signup'),
     path('',views.Home.as_view(),name='home'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/login_signup.html'),name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,20 +2,38 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from .models import Profile
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
 # Create your views here.
 
 class Home(TemplateView):
 	template_name='users/home.html'
 
-def signup(request):
-	if request.method=='POST' and 'signup' in request.POST:
+def login_signup(request):
+	print('enter')
+	if request.method=='POST' and 'login' in request.POST:
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		print(username)
+		user=authenticate(username=username,password=password)
+		if user:
+			if user.is_active:
+				login(request,user)
+				return redirect('home')
+		else:
+			return redirect('login_signup')
+
+
+
+
+	elif request.method=='POST' and 'signup' in request.POST:
 		first_name=request.POST.get('first_name')
 		last_name=request.POST.get('last_name')
 		email=request.POST.get('email')
 		password=request.POST.get('password')
 		dob=request.POST.get('DOB')
 		gender=request.POST.get('gender')
-
 		user=User(username=email,first_name=first_name,last_name=last_name,email=email,password=password)
 		profile=Profile(dob=dob,gender=gender)
 
@@ -25,3 +43,6 @@ def signup(request):
 		return redirect('home')
 
 	return render(request,'users/login_signup.html')
+
+
+					
