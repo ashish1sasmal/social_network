23d9f12f3a588103c3b56270429192df2a8748cb
ashish1sasmal <ashishsasmal1@gmail.com>
2019-09-08 19:20:04 +0530

    made profile display

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 
 from . import views
 urlpatterns = [
-    path('',views.edit_profile,name='profile'),
+    path('profile/edit/',views.edit_profile,name='edit-profile'),
+    path('profile/',views.ProfileView.as_view(),name='profile'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.template import RequestContext
 # Create your views here.
+
+
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+	template_name='userprofile/profile.html'
+	context_object_name = 'user'
+	def get_queryset(self):
+		return User.objects.filter(user=self.request.user)
+
 
 @login_required
 def edit_profile(request):
